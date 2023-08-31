dd66f77f10321a56bad905a4ed6208b13f16d813
Johan Mattsson <johan.mattsson@hotmail.se>
2023-08-31 18:12:27 +0200

    Create Position class

---
+++ ClassLibrary1/Position.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Represents a position in 2D space.
+    /// (Has a X and Y coordinate.)
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// X-coordinate of this position.
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Y-coordinate of this position.
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        /// Create a new position with given coordinates.
+        /// </summary>
+        /// <param name="x">X-coordinate of position</param>
+        /// <param name="y">Y-coordinate of position</param>
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ TestProject1/PositionTests.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void NewPositionShouldSetXCoordinate()
+        {
+            Position position = new(3, 5);
+            Assert.Equal(3, position.X);
+        }
+
+        [Fact]
+        public void NewPositionShouldSetYCoordinate()
+        {
+            Position position = new(3, 5);
+            Assert.Equal(5, position.Y);
+        }
+    }
+}
