7c8f9a74791ba99508a8ab5a8a464f124d27c724
Johan Mattsson <johan.mattsson@hotmail.se>
2023-09-01 16:42:06 +0200

    Create Simulator.cs

---
+++ ClassLibrary1/Simulator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Simulator
+    {
+        /// <summary>
+        /// Simulate a moving object on a table.
+        /// </summary>
+        /// <param name="table">The table the object is moving on</param>
+        /// <param name="startPositionX">Initial X-coordinate of moving object</param>
+        /// <param name="startPositionY">Initial Y-coordinate of moving object</param>
+        /// <param name="commands">The commands to execute</param>
+        /// <returns>The final position of the object after executing the commands</returns>
+        /// <exception cref="ArgumentException">If starting position is illegal on this table</exception>
+        public static Position Simulate(ITable table, int startPositionX, int startPositionY, Command[] commands)
+        {
+            Position startPosition = new(startPositionX, startPositionY);
+            MovingObject movingObject = new(startPosition);
+
+            if (!table.IsValidPosition(startPosition))
+            {
+                throw new ArgumentException($"Illegal start position on table: {startPosition}");
+            }
+
+            foreach (var command in commands)
+            {
+                switch (command)
+                {
+                    case Command.MoveForward:
+                        movingObject.Move(MoveDirection.Forward);
+                        break;
+                    case Command.MoveBackward:
+                        movingObject.Move(MoveDirection.Backward);
+                        break;
+                    case Command.TurnLeft:
+                        movingObject.Turn(TurnDirection.Left);
+                        break;
+                    case Command.TurnRight:
+                        movingObject.Turn(TurnDirection.Right);
+                        break;
+                    case Command.Quit:
+                        return movingObject.CurrentPosition;
+                }
+            }
+
+            return movingObject.CurrentPosition;
+        }
+    }
+}
---
+++ TestProject1/SimulatorTests.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class SimulatorTests
+    {
+        [Theory]
+        [InlineData(3, 5, 0, 1, new Command[] { }, 0, 1)]
+        [InlineData(3, 5, 0, 1, new Command[] { Command.Quit }, 0, 1)]
+        [InlineData(3, 5, 2, 4, new Command[] { Command.MoveForward }, 2, 3)]
+        [InlineData(3, 5, 0, 1, new Command[] { Command.MoveForward, Command.Quit, Command.MoveBackward }, 0, 0)]
+        [InlineData(4, 4, 2, 2, new Command[] { Command.MoveForward, Command.TurnLeft, Command.MoveForward, Command.TurnRight, Command.MoveBackward, Command.TurnRight, Command.MoveBackward, Command.TurnLeft, Command.MoveForward, Command.Quit}, 0, 1)]
+        public void TestSimulationWithRectangularTable(int tableWidth, int tableHeight, int startPositionX, int startPositionY, Command[] commands, int finalPositionX, int finalPositionY)
+        {
+            ITable table = new RectangularTable(tableWidth, tableHeight);
+
+            Position expect = new(finalPositionX, finalPositionY);
+
+            Assert.Equal(expect, Simulator.Simulate(table, startPositionX, startPositionY, commands));
+        }
+    }
+}
