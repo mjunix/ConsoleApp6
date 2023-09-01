1a15c2fc76ded5e943691249374ce21d135ca865
Johan Mattsson <johan.mattsson@hotmail.se>
2023-09-01 21:43:05 +0200

    Create InvalidPositionException (to be used when object reaches invalid position on table)

---
+++ ClassLibrary1/InvalidPositionException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class InvalidPositionException : Exception
+    {
+        public InvalidPositionException()
+        {
+        }
+
+        public InvalidPositionException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidPositionException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
