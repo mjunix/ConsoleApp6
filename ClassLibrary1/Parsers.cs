a45dc84746db0e752fe12c54b75d7b816dcdded3
Johan Mattsson <johan.mattsson@hotmail.se>
2023-09-02 01:07:48 +0200

    Comment Parsers.cs

--- ClassLibrary1/Parsers.cs
+++ ClassLibrary1/Parsers.cs
@@ -8,6 +8,13 @@ namespace ClassLibrary1
 {
     public class Parsers
     {
+        /// <summary>
+        /// Parse a string representing a header.
+        /// A header string consists of four integers seperaded by space or commas: width, height, X-position, Y-position
+        /// </summary>
+        /// <param name="line">String that will be parsed as a header</param>
+        /// <returns>Tuple: (tableWidth, tableHeight, position)</returns>
+        /// <exception cref="Exception">Throws Exception on invalid header</exception>
         public static (int tableWidth, int tableHeight, Position position) ParseHeader(string line)
         {
             var tokens = line.Split(new char[] { ' ', '\t', ',' });
@@ -32,6 +39,12 @@ namespace ClassLibrary1
             return (tableWidth: intValues[0], tableHeight: intValues[1], position: new Position(intValues[2], intValues[3]));
         }
 
+        /// <summary>
+        /// Parse a string containing commands to be executed.
+        /// </summary>
+        /// <param name="line">String to be parsed as commands, a command is an integer, the commands are separated by space or commas</param>
+        /// <returns>Array of commands</returns>
+        /// <exception cref="Exception">Throws Exception on invalid commands</exception>
         public static Command[] ParseCommands(string line)
         {
             var tokens = line.Split(new char[] { ' ', '\t', ',' });
