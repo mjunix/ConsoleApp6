5a6506b59e5e8eba528a42f4296b89dced1c1e88
Johan Mattsson <johan.mattsson@hotmail.se>
2023-09-01 18:20:38 +0200

    Create Parsers.cs

---
+++ ClassLibrary1/Parsers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Parsers
+    {
+        public static (int tableWidth, int tableHeight, Position position) ParseHeader(string line)
+        {
+            var tokens = line.Split(new char[] { ' ', '\t', ',' });
+
+            if (tokens.Length != 4)
+            {
+                throw new Exception("Invalid header, expected 4 integers");
+            }
+
+            var intValues = tokens.Select(token =>
+            {
+                if (int.TryParse(token, out int result))
+                {
+                    return result;
+                }
+                else
+                {
+                    throw new Exception($"Invalid token, expected integer but got: \"{token}\"");
+                }
+            }).ToArray();
+
+            return (tableWidth: intValues[0], tableHeight: intValues[1], position: new Position(intValues[2], intValues[3]));
+        }
+
+        public static Command[] ParseCommands(string line)
+        {
+            var tokens = line.Split(new char[] { ' ', '\t', ',' });
+
+            var commands = tokens.Select(token =>
+            {
+                if (int.TryParse(token, out int result))
+                {
+                    return (Command)result;
+                }
+                else
+                {
+                    throw new Exception($"Invalid command: {token}");
+                }
+            }).ToArray();
+
+            return commands;
+        }
+    }
+}
---
+++ TestProject1/ParsersTest.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class ParsersTest
+    {
+        [Theory]
+        [InlineData("4,4,2,2", 4, 4, 2, 2)]
+        [InlineData("4,3,2,1", 4, 3, 2, 1)]
+        [InlineData("4 4 2 2", 4, 4, 2, 2)]
+        [InlineData("4 3 2 1", 4, 3, 2, 1)]
+        public void Test_ParseHeader(string line, int expectTableWidth, int expectTableHeight, int expectPositionX, int expectPositionY)
+        {
+            var expect = (expectTableWidth, expectTableHeight, new Position(expectPositionX, expectPositionY));
+            Assert.Equal(expect, Parsers.ParseHeader(line));
+        }
+
+        [Theory]
+        [InlineData("1,4,1,3,2,3,2,4,1,0", new Command[] { Command.MoveForward, Command.TurnLeft, Command.MoveForward, Command.TurnRight, Command.MoveBackward, Command.TurnRight, Command.MoveBackward, Command.TurnLeft, Command.MoveForward, Command.Quit })]
+        [InlineData("1 4 1 3 2 3 2 4 1 0", new Command[] { Command.MoveForward, Command.TurnLeft, Command.MoveForward, Command.TurnRight, Command.MoveBackward, Command.TurnRight, Command.MoveBackward, Command.TurnLeft, Command.MoveForward, Command.Quit })]
+        public void Test_ParseCommads(string line, Command[] expect)
+        {
+            Assert.Equal(expect, Parsers.ParseCommands(line));
+        }
+    }
+}
