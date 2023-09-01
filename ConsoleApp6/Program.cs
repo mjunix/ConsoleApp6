ece386504577127e036dcf39ceb189b74531a6d4
Johan Mattsson <johan.mattsson@hotmail.se>
2023-09-01 22:24:54 +0200

    Convert Main() to top-level statements

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,46 +1,37 @@
 ﻿using ClassLibrary1;
 
-namespace ConsoleApp6
+Position FailurePosition = new(-1, -1);
+
+// Read the first line (header)
+var line = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(line))
 {
-    internal class Program
-    {
-        private static readonly Position FailurePosition = new(-1, -1);
-
-        static void Main(string[] args)
-        {
-            // Read the first line (header)
-            var line = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(line))
-            {
-                return;
-            }
-
-            var (tableWidth, tableHeight, startPosition) = Parsers.ParseHeader(line);
-
-            List<Command> commands = new();
-
-            // Read the remaining  line(s) which are the commands...
-            line = Console.ReadLine();
-
-            while (!string.IsNullOrWhiteSpace(line))
-            {
-                commands.AddRange(Parsers.ParseCommands(line));
-                line = Console.ReadLine();
-            }
-
-            Position? result;
-
-            try
-            {
-                result = Simulator.Simulate(new RectangularTable(tableWidth, tableHeight), startPosition, commands.ToArray());
-            }
-            catch (InvalidPositionException)
-            {
-                result = FailurePosition;
-            }
-
-            Console.WriteLine(result.ToString());
-        }
-    }
+    return;
 }
+
+var (tableWidth, tableHeight, startPosition) = Parsers.ParseHeader(line);
+
+List<Command> commands = new();
+
+// Read the remaining  line(s) which are the commands...
+line = Console.ReadLine();
+
+while (!string.IsNullOrWhiteSpace(line))
+{
+    commands.AddRange(Parsers.ParseCommands(line));
+    line = Console.ReadLine();
+}
+
+Position? result;
+
+try
+{
+    result = Simulator.Simulate(new RectangularTable(tableWidth, tableHeight), startPosition, commands.ToArray());
+}
+catch (InvalidPositionException)
+{
+    result = FailurePosition;
+}
+
+Console.WriteLine(result.ToString());
