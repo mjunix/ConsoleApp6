ffcb83853967032fa13d83af22b8630478c8f939
Johan Mattsson <johan.mattsson@hotmail.se>
2023-09-02 15:36:20 +0200

    Fix error handling and only read two lines from stdin

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -2,36 +2,47 @@
 
 Position FailurePosition = new(-1, -1);
 
-// Read the first line (header)
-var line = Console.ReadLine();
-
-if (string.IsNullOrWhiteSpace(line))
-{
-    return;
-}
-
-var (tableWidth, tableHeight, startPosition) = Parsers.ParseHeader(line);
-
-List<Command> commands = new();
-
-// Read the remaining  line(s) which are the commands...
-line = Console.ReadLine();
-
-while (!string.IsNullOrWhiteSpace(line))
-{
-    commands.AddRange(Parsers.ParseCommands(line));
-    line = Console.ReadLine();
-}
-
-Position? result;
+Position result = FailurePosition;
 
 try
 {
-    result = Simulator.Simulate(new RectangularTable(tableWidth, tableHeight), startPosition, commands.ToArray());
+    // Read the first line (header)
+    var line = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        return;
+    }
+
+    var (tableWidth, tableHeight, startPosition) = Parsers.ParseHeader(line);
+    ITable table = new RectangularTable(tableWidth, tableHeight);
+
+    if (!table.IsValidPosition(startPosition))
+    {
+        throw new InvalidPositionException($"Invalid start position: {startPosition}");
+    }
+
+    List<Command> commands = new();
+
+    // Read the next line which are the commands...
+    line = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        commands.AddRange(Parsers.ParseCommands(line));
+    }
+
+    // perform the actual simulation
+    result = Simulator.Simulate(table, startPosition, commands.ToArray());
 }
 catch (InvalidPositionException)
 {
-    result = FailurePosition;
+    // Intentionally left blank, default value of "result" ([-1, -1]) will be printed at end of program
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"ERROR: {ex.Message}");
+    return;
 }
 
 Console.WriteLine(result.ToString());
